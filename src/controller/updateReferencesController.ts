6229c2d0cc2f755a149c820394d26ae991d294c0
WorkAttack <workattackgeral@gmail.com>
2023-12-15 20:22:11 +0100

    Update dependencies and modify updateReferencesController

--- src/controller/updateReferencesController.ts
+++ src/controller/updateReferencesController.ts
@@ -4,14 +4,41 @@ import { SimpleCostumerType, simpleCostumerSchema } from "../models/Costumer";
 import createProxyPayReferenceService from "../services/createProxyPayReferenceService";
 import { z } from "zod";
 
+type SimpleCostumerWithNameType = SimpleCostumerType & {
+  name: string;
+};
+
+const paramsSchema = z.object({
+  limit: z.coerce
+    .number()
+    .min(1, "limit must be at least 1")
+    .max(100, "limit cannot exceed 100"),
+  offset: z.coerce.number().min(0, "offset must be at least 0").default(0),
+});
+
 export const updateReferencesController = async (
   req: Request,
   res: Response
 ) => {
   try {
-    const customers = (
-      await (await getSplynxApi()).get("admin/customers/customer")
-    )?.response as any[];
+    const params = paramsSchema.safeParse(req.query);
+
+    if (!params.success) {
+      return res.status(400).json({ message: params.error });
+    }
+
+    const { limit, offset } = params.data;
+
+    const customersList = (
+      (await (await getSplynxApi()).get("admin/customers/customer"))
+        ?.response as any[]
+    ).filter((c) => !isNaN(parseInt(c.login)));
+
+    if (!customersList?.length) {
+      return res.status(404).json({ message: "no customers found" });
+    }
+
+    const customers = customersList.slice(offset, offset + limit);
 
     if (!customers?.length) {
       return res.status(404).json({ message: "no customers found" });
@@ -19,23 +46,37 @@ export const updateReferencesController = async (
 
     const validCustomers = z.array(simpleCostumerSchema).parse(customers);
 
-    type SimpleCostumerWithNameType = SimpleCostumerType & {
-      name: string;
-    };
+    const results = await Promise.allSettled(
+      validCustomers.map((c) => createProxyPayReferenceService(c))
+    );
 
-    const failedCustomers: SimpleCostumerWithNameType[] = [];
-    const successCustomers: SimpleCostumerWithNameType[] = [];
-
-    for await (const c of validCustomers) {
-      if (isNaN(parseInt(c.login))) continue;
-      await createProxyPayReferenceService(c)
-        .catch(() => {
-          failedCustomers.push({ ...c, name: c.login });
-        })
-        .then(() => {
-          successCustomers.push({ ...c, name: c.login });
-        });
-    }
+    const { failedCustomers, successCustomers } = results.reduce(
+      (
+        acc: {
+          successCustomers: SimpleCostumerWithNameType[];
+          failedCustomers: SimpleCostumerWithNameType[];
+        },
+        result,
+        index
+      ) => {
+        const cos = customers[index];
+        if (result.status === "fulfilled") {
+          acc.successCustomers.push({
+            id: cos.id,
+            login: cos.login,
+            name: cos.name,
+          });
+        } else {
+          acc.failedCustomers.push({
+            id: cos.id,
+            login: cos.login,
+            name: cos.name,
+          });
+        }
+        return acc;
+      },
+      { failedCustomers: [], successCustomers: [] }
+    );
 
     return res.json({
       message: `${successCustomers.length} were updated successfully and ${
