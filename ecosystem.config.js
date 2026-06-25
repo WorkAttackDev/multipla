d19c7a579a3bf83068c50309aaa111601798eb7c
Denilson Costa <workattackgeral@gmail.com>
2026-06-25 15:28:00 +0100

    feat: add reconcile script for development environment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,5 +5,14 @@ module.exports = {
       name: process.env.APP_NAME,
       script: "npm start",
     },
+    {
+      name: "izinet-reconcile",
+      script: "./build/scripts/reconcile.js",
+      cron_restart: "0 0 * * *",
+      autorestart: false,
+      env: {
+        NODE_ENV: "production",
+      },
+    },
   ],
 };
